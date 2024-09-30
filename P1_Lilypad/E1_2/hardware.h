66df14d382ebfa06557463282ce78f25eecba3ec
cristhian <cm.valenzuela@alumnos.upm.es>
2024-09-30 12:20:44 +0200

    hardware.h

---
+++ P1_Lilypad/E1_2/hardware.h
@@ -0,0 +1,24 @@
+#ifndef HARDWARE_H 
+#define HARDWARE_H
+
+#define LIGHT_SENSOR 	2
+#define BUUZZER			3
+#define BUTTON			4
+#define LED_YELLOW		5
+#define LED_RED			6
+#define LED_GREEN		7
+#define LED_BLUE		8		
+#define	SWITCH			9
+#define I2C_SCL			10
+#define I2C_SDA			11
+#define	RGB_RED			12
+#define RGB_GREEN		13
+#define RGB_BLUE		14
+#define BARRA_LED0		15
+#define BARRA_LED1		16
+#define BARRA_LED2		17
+#define BARRA_LED3		18
+#define BARRA_LED4		19
+#define BARRA_LED5		20
+
+#endif
