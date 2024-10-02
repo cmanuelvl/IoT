0be0d5d2cd6278f78e5dc942c91ba5aea9b493ad
Camilo Zuñiga <camilo.zflor@alumnos.upm.es>
2024-10-02 13:20:53 +0200

    Automata Principal

--- P1_Lilypad/E1_2/E1_2.ino
+++ P1_Lilypad/E1_2/E1_2.ino
@@ -8,11 +8,14 @@
 
 #define TMR_500ms 500
 #define TMR_400ms 400
+#define UMBRAL 50
 
 const int ledPins[6] = {BG_LED0, BG_LED1, BG_LED2, BG_LED3, BG_LED4, BG_LED5};
+const int estado[3] = {INICIAL, UMBRAL, ALARMA};
 
 BarraLeds barraLeds(ledPins,6);
 LightSensor lightSensor(LIGHT_SENSOR, 3.3, 10);
+int barra_value;
 Buzzer buzzer(BUZZER);
 Button button(BUTTON);
 
@@ -33,17 +36,50 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  tmrid_500ms.update();
-  tmrid_400ms.update();
-  if (button.buttonRead() == 0){
-    buzzer.buzzerOn(350);
-    delay(1000);
-    buzzer.buzzerOff();
+  switch(estado){
+    case INICIAL: // Estado inicial, se realiza la medida de la luminancia cada 400 ms y se refleja en la barra 
+      light_measure();
+      
+      if(barra_value >= UMBRAL){
+        estado = UMBRAL;
+        // Lanzar un temporizador que cuente tiempo
+      }
+    break; 
+
+    case UMBRAL: // Estado UMBRAL superado, se empieza a contar el tiempo de exposicion, a la mitad del tiempo (20s/2) se enciende RGB
+       light_measure();
+       // Cuando se lleven 10s encender rgb, y cuando se lleven 20s pasamos al estado ALARMA enciendo el altavoz 
+       
+       if (barra_value < UMBRAL){
+        estado = INICIAL;
+        // Borrar la temporizacion
+       }
+    break;
+
+    case ALARMA: // Estado ALARMA encendida, se activa el altavoz porque se ha superado 20s por encima del UMBRAL, solo se puede apagar mediante el pulsador
+
+    break;
   }
-}
+  
+//  tmrid_500ms.update();
+//  tmrid_400ms.update();
+//  if (button.buttonRead() == 0){
+//    buzzer.buzzerOn(350);
+//    delay(1000);
+//    buzzer.buzzerOff();
+//  }
+//}
 
 void tmr_Callback_500ms(){
 }
 
 void tmr_Callback_400ms(){
+  barra_value = lightSensor.getScaledValue();
+  barraLeds.barraLedOn(barra_value);
+}
+
+void light_measure(){
+  delay(400);
+  barra_value = lightSensor.getScaledValue();
+  barraLeds.barraLedOn(barra_value);
 }
