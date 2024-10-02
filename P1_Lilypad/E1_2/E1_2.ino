3a18c365e66f216cab33cb2a1fff96cae959e6e8
Camilo Zuñiga <camilo.zflor@alumnos.upm.es>
2024-10-02 15:22:42 +0200

    Automata Principal base

--- P1_Lilypad/E1_2/E1_2.ino
+++ P1_Lilypad/E1_2/E1_2.ino
@@ -8,6 +8,8 @@
 
 #define TMR_500ms 500
 #define TMR_400ms 400
+#define TMR_10000ms 10000
+#define TMR_20000ms 20000
 #define UMBRAL 50
 
 const int ledPins[6] = {BG_LED0, BG_LED1, BG_LED2, BG_LED3, BG_LED4, BG_LED5};
@@ -21,6 +23,8 @@ Button button(BUTTON);
 
 TimerEvent tmrid_500ms;
 TimerEvent tmrid_400ms;
+TimerEvent tmrid_10000ms;
+TimerEvent tmrid_20000ms;
 
 void setup() {
   // put your setup code here, to run once:
@@ -42,13 +46,15 @@ void loop() {
       
       if(barra_value >= UMBRAL){
         estado = UMBRAL;
-        // Lanzar un temporizador que cuente tiempo
+        tmrid_10000ms.set(TMR_10000ms, tmr_Callback_10000ms);// Lanzar un temporizador que cuente tiempo de 10s
+        tmrid_20000ms.set(TMR_20000ms, tmr_Callback_20000ms);// Lanzar un temporizador que cuente tiempo de 20s
       }
     break; 
 
     case UMBRAL: // Estado UMBRAL superado, se empieza a contar el tiempo de exposicion, a la mitad del tiempo (20s/2) se enciende RGB
        light_measure();
        // Cuando se lleven 10s encender rgb, y cuando se lleven 20s pasamos al estado ALARMA enciendo el altavoz 
+       
        
        if (barra_value < UMBRAL){
         estado = INICIAL;
@@ -57,7 +63,9 @@ void loop() {
     break;
 
     case ALARMA: // Estado ALARMA encendida, se activa el altavoz porque se ha superado 20s por encima del UMBRAL, solo se puede apagar mediante el pulsador
-
+      if (button.buttonRead() == 0){
+        estado = INICIO;
+      }
     break;
   }
   
@@ -73,9 +81,20 @@ void loop() {
 void tmr_Callback_500ms(){
 }
 
-void tmr_Callback_400ms(){
-  barra_value = lightSensor.getScaledValue();
-  barraLeds.barraLedOn(barra_value);
+//void tmr_Callback_400ms(){
+//  barra_value = lightSensor.getScaledValue();
+//  barraLeds.barraLedOn(barra_value);
+//}
+
+void tmr_Callback_10000ms(){
+  // Encender RGB
+}
+
+void tmr_Callback_20000ms(){
+  // Encender altavoz
+  buzzer.buzzerOn(350);
+  delay(1000);
+  buzzer.buzzerOff();
 }
 
 void light_measure(){
