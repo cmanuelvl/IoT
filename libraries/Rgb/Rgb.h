52b4df4786cf4ed01a425ba755f3c41e8311dca3
cristhian <cm.valenzuela@alumnos.upm.es>
2024-10-03 15:46:58 +0200

    principal (modificado), rgb

---
+++ libraries/Rgb/Rgb.h
@@ -0,0 +1,18 @@
+#ifndef RGB_H
+#define RGB_H
+
+class Rgb {
+	public:
+		// Constructor
+		Rgb(int pins[]);
+
+		// Métodos
+		void initialize();
+		void rgbColor(int redValue, int greenValue, int blueValue);
+		
+	private:
+		int my_pins[3];					//conexionado de pines
+		
+};
+
+#endif
