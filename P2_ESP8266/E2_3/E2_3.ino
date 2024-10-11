52b317d700d4535207a0dd0b1fe3cd6d211249a7
Camilo Zuñiga <camilo.zflor@alumnos.upm.es>
2024-10-11 12:33:08 +0200

    Probar funcionamiento

--- P2_ESP8266/E2_3/E2_3.ino
+++ P2_ESP8266/E2_3/E2_3.ino
@@ -4,6 +4,27 @@
 #include <WiFiUdp.h>
 //#include <ESP8266mDNS.h>
 
+#include <Buzzer.h>
+#include <hardware.h>
+#include <TimeOut.h>
+
+// define
+#define TMR_1s   1000
+#define TMR_2s   2000
+
+// Objetos 
+Buzzer oBuzzer(PIN_BUZZER);
+TimeOut tmrid_1s;
+TimeOut tmrid_2s;
+
+// Typedef
+typedef enum {NORMAL, ALARMA_RGB, ALARMA_BUZZER, WAIT} t_estados;
+t_estados estado;
+
+// Variables 
+bool buzzerOn;
+int cuenta = 0; // Para contar las tres rafagas del altavoz 
+
 const char *ssid = " ";
 const char *password = "              ";
 
@@ -13,6 +34,13 @@ ESP8266WebServer server ( 80 );
 //const int led2 = 14;
 
 String webPage;
+
+// Function prototype 
+void paginaNoEncontrada();
+void Led_Blue_Init();
+void tmr_Callback_1s();
+void tmr_Callback_2s();
+
 
 // Funcion que proporciona un mensaje detallado cuando se acceda a una ruta no encontrada en el servidor web del ESP8266
 void paginaNoEncontrada() {
@@ -41,6 +69,12 @@ void setup() {
 //  pinMode ( led2, OUTPUT );
 //  digitalWrite ( ledl, 1);
 //  digitalWrite ( led2, 1);
+
+  // Inicializacion perifericos 
+  Led_Blue_Init();
+  obuzzer.initialize();
+
+  // Inicializacion WIFI
   Serial.begin ( 115200 );
   WiFi.begin ( ssid, password );
   Serial.println ( "" );
@@ -56,6 +90,11 @@ void setup() {
   Serial.print ( "IP address: " ) ;
   Serial.println ( WiFi. localIP() ) ;
 
+  digitalWrite(PIN_LED_B, HIGH); // Encender lez azul para notificar la conexion 
+  buzzerOn = true; // Encender Altavoz 
+  oBuzzer.buzzerToggle(350, 500);
+  tmrid_1s.timeOut(TMR_1s, tmr_Callback_1s); 
+  
   //digitalWrite ( ledl, 0);
 
   webPage= "<p style=\"padding-left: 120px;\"><span style=\"text-decoration: underline; color: #ff0000;\"><strong>ESTE ES MI SERVER</strong></span></p>";
@@ -73,6 +112,7 @@ void setup() {
 
   server.on("/Off", [](){
     //digitalWrite ( led2, 1);
+    digitalWrite(PIN_LED_B, LOW); // Apagar lez azul para notificar la desconexion
     server.send(200, "text/html", webPage); // Envio del archivo html pidiendo contrasena
   });
 
@@ -84,4 +124,26 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   server.handleClient(); // Procesa las solicitudes HTTP de los clientes 
+}
+
+// Inicializacion Led Blue en modo Salida
+void Led_Blue_Init(){
+  pinMode(PIN_LED_B, OUTPUT);
+}
+
+void tmr_Callback_1s(){
+  obuzzer.BuzzerOff(); 
+  tmrid_2s.timeOut(TMR_2s, tmr_Callback_2s); 
+}
+
+void tmr_Callback_2s(){
+  buzzerOn = true;
+  oBuzzer.buzzerToggle(350, 500);
+  cuenta = cuenta + 1;
+  if (cuenta < 3){
+    tmrid_1s.timeOut(TMR_1s, tmr_Callback_1s);
+  }else { // cuenta == 3
+    obuzzer.BuzzerOff(); 
+  }
+   
 }
